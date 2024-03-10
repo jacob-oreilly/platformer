72d5d0603e3de8e53d26a14ace65cde9a30573b9
Jake <jakeor45@gmail.com>
2024-03-09 17:16:41 -0800

    Figured out AABB collision. Easiest way to do it is size your game objects with scales.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use bevy::{math::bounding::{Aabb2d, BoundingVolume, IntersectsVolume}, prelude::*, sprite::{MaterialMesh2dBundle, Mesh2dHandle}, window::PrimaryWindow};
+use bevy::{math::bounding::{Aabb2d, Bounded2d, BoundingVolume, IntersectsVolume}, prelude::*, sprite::{MaterialMesh2dBundle, Mesh2dHandle}, window::PrimaryWindow};
 
 #[derive(Component)]
 struct Player{
     movement_speed: f32,
     velocity: Vec3,
+    is_colliding: bool,
 }
 
 #[derive(Component)]
@@ -23,12 +24,14 @@ enum Collision {
     Bottom,
 }
 
+const GRAVITY: f32 = 9.821 * 50.0;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (camera_setup, player_setup, platform_setup))
-        .add_systems(FixedUpdate, (player_update, check_for_collisions).chain())
         .add_event::<CollisionEvent>()
+        .add_systems(Startup, (camera_setup, player_setup, platform_setup))
+        .add_systems(Update,(check_for_collisions, player_update).chain())
         .run();
 }
 
@@ -54,16 +57,16 @@ fn player_setup(
     let window = window_query.get_single().unwrap();
     commands.spawn((
         MaterialMesh2dBundle{
-            mesh: Mesh2dHandle(meshes.add(Capsule2d::new(15.0, 35.0))),
+            mesh: Mesh2dHandle(meshes.add(Capsule2d::new(1.0, 1.0))),
             material: materials.add(Color::FUCHSIA),
-            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            transform: Transform::from_xyz(window.width() / 2.0, window.height(), 0.0).with_scale(Vec3::new(20.0, 25.0, 1.0)),
             ..default()
         },
         Player {
             movement_speed: 500.0,
-            velocity: Vec3::new(0.0, 0.0, 0.0)
+            velocity: Vec3::new(0.0, 0.0, 0.0),
+            is_colliding: false
         },
-        Collider,
     ));
 }
 
@@ -77,9 +80,9 @@ fn platform_setup(
     let window = window_query.get_single().unwrap();
     commands.spawn((
         MaterialMesh2dBundle{
-            mesh: Mesh2dHandle(meshes.add(Rectangle::new(200.0, 20.0))),
+            mesh: Mesh2dHandle(meshes.add(Rectangle::new(1.0, 1.0))),
             material: materials.add(Color::GRAY),
-            transform: Transform::from_xyz(window.width() / 2.0, (window.height() / 2.0) - 100.0, 0.0),
+            transform: Transform::from_xyz(window.width() / 2.0, (window.height() / 2.0) - 200.0, 0.0).with_scale(Vec3::new(200.0, 20.0, 0.0)),
             ..default()
         },
         Platform {
@@ -94,17 +97,25 @@ fn player_update(
     mut player_query: Query<(&mut Transform, &mut Player), With<Player>>,
     time: Res<Time>,
 ) {
-    let acceleration: f32 = 9.8;
+    let acceleration: f32 = 1.0;
     if let Ok((mut transform, mut player)) = player_query.get_single_mut() {
+        let mut direction = Vec3::new(0.0, 0.0, 0.0);
         let dt = time.delta_seconds();
-        player.velocity.y += acceleration * dt;
-        let y_direction =  player.velocity.y * dt - transform.translation.y;
-        let mut direction = Vec3::new(0.0, y_direction, 0.0);
+        
+        
+        if !player.is_colliding {
+            // let mut y_pos = transform.translation.y;
+            // // player.velocity.y += acceleration * dt;
+            // // y_pos += player.velocity.y * dt;
+            // y_pos += acceleration * dt;
+            // direction.y = -y_pos;
+            transform.translation.y -= GRAVITY * dt;
+        }
         if keys.pressed(KeyCode::ArrowLeft) || keys.pressed(KeyCode::KeyA) {
-            direction += Vec3::new(-1.0, y_direction, 0.0);
+            direction.x += -1.0;
         }
         if keys.pressed(KeyCode::ArrowRight) || keys.pressed(KeyCode::KeyD) {
-            direction += Vec3::new(1.0, y_direction, 0.0);
+            direction.x += 1.0;
         }
         if direction.length() > 0.0 {
             direction = direction.normalize();
@@ -114,25 +125,31 @@ fn player_update(
 }
 
 fn check_for_collisions(
-    mut player_query: Query<(&Transform, &mut Player), With<Player>>,
+    mut player_query: Query<(&Transform, Entity, &mut Player), With<Player>>,
     collider_query: Query<&Transform, With<Collider>>,
+    platform_query: Query<&Transform, With<Platform>>,
     mut collision_events: EventWriter<CollisionEvent>
 ) {
-    let (player_transform, mut player) = player_query.single_mut();
+    let (player_transform, player_entity, mut player) = player_query.single_mut();
 
     for collider_transform in &collider_query {
         let collision = platform_collision(
-            Aabb2d::new(player_transform.translation.truncate(), player_transform.scale.truncate() / 2.),
+            Aabb2d::new(player_transform.translation.truncate(), player_transform.scale.truncate() / 2.0),
             Aabb2d::new(
                 collider_transform.translation.truncate(),
                 collider_transform.scale.truncate() / 2.,
             ),
         );
-
+       
+        println!("Player Transform: {:?}", player_transform);
+        println!("collider_transform: {:?}", collider_transform);
+        
         if let Some(collision) = collision {
+            println!("Collision: {:?}", collision);
+            player.is_colliding = true;
             // Sends a collision event so that other systems can react to the collision
             collision_events.send_default();
-
+            println!("Colliding here");
             // reflect the ball when it collides
             let mut reflect_x = false;
             let mut reflect_y = false;
@@ -156,13 +173,17 @@ fn check_for_collisions(
                 player.velocity.y = 0.0
             }
         }
+        else {
+            player.is_colliding = false;
+        }
     }
 }
 
 fn platform_collision(
     player: Aabb2d, platform: Aabb2d
 ) -> Option<Collision> {
-    if !player.intersects(&platform) {
+    if !&player.intersects(&platform) {
+        // println!("Player {:?}, Platform {:?}", player, &platform);
         return None;
     }
 
@@ -179,6 +200,7 @@ fn platform_collision(
     } else {
         Collision::Bottom
     };
+
 
     Some(current_platform)
 }
